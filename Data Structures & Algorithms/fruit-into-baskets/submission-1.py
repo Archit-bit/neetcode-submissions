bf1c1042a24a1b5714b226f0f2ec5007da686ff8
Arc-hit <60813780+Archit-bit@users.noreply.github.com>
2026-04-07 14:43:07 +0530

    Add: fruit-into-baskets - submission-1

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        l=0
+        count=collections.defaultdict(int)
+        #total=0
+        res=0
+        for r in range(len(fruits)):
+            count[fruits[r]]+=1
+            
+            while len(count)>2:
+                count[fruits[l]]-=1
+                
+                
+                if not count[fruits[l]]:
+                    count.pop(fruits[l])
+                l+=1
+            res=max(res,r-l+1)
+        return res
+
+        
