10c3e3e6937d931b3cd56373ee97a9b9cb637f1c
Arc-hit <60813780+Archit-bit@users.noreply.github.com>
2026-04-18 09:54:42 +0530

    Add: products-of-array-discluding-self - submission-26

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-26.py
@@ -0,0 +1,13 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        p=1
+        output=[]
+        for i in range(len(nums)):
+            output.append(p)
+            p*=nums[i]
+        p=1
+        for i in range(len(nums)-1,-1,-1):
+            output[i]*=p
+            p*=nums[i]
+        return output
+        
