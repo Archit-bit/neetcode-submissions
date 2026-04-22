4cce60c1ea6337d606964993b4015452d2166a29
Arc-hit <60813780+Archit-bit@users.noreply.github.com>
2026-04-23 02:24:57 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        freq={}
+        for char in s:
+            freq[char]=freq.get(char,0)+1
+        for char in t:
+            if char in freq:
+                freq[char]-=1
+                if freq[char]<=0:
+                    del freq[char]
+            else:
+                return False
+        return True
+        
