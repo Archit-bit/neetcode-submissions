df54d9acb060cc79ff76c770fafa0473486a5432
Arc-hit <60813780+Archit-bit@users.noreply.github.com>
2026-04-25 20:08:11 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res=cur_sum=0
+        prefix={0:1}
+        for num in nums:
+            cur_sum+=num
+            dif=cur_sum-k
+            res+=prefix.get(dif,0)
+            prefix[cur_sum]=1+prefix.get(cur_sum,0)
+        return res
+        
+        
+        
